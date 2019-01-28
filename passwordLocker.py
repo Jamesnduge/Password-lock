f528a4a2f13ba75731d39043d5923be7dd553a42
James Mwangi <james_mwangi@aol.com>
2019-01-28 23:14:52 +0300

    add a new credentials class with 3 arguements

--- passwordLocker.py
+++ passwordLocker.py
@@ -4,3 +4,15 @@ class User:
     def __init__(self, username, password):
         self.username = username
         self.password = password
+
+    def save_user(self):
+        User.users_list.append(self)
+
+
+
+class Credentials:
+    accounts_list = []
+    def __init__(self, account_name, user_name, pass):
+        self.account_name = account_name
+        self.user_name = user_name
+        self.pass = pass
