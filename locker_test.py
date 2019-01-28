8fa1e3bae5dc56357ba74160b9af5ad036b17921
James Mwangi <james_mwangi@aol.com>
2019-01-28 23:02:34 +0300

    add class user and start tests

---
+++ locker_test.py
@@ -0,0 +1,20 @@
+import unittest
+from passwordLocker import User
+
+class TestLocker(unittest.TestCase):
+
+    '''
+    Test class that defines test cases for the user class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
+    def tearDown(self):
+            '''
+            tearDown method that does clean up after each test case has run.
+            '''
+            User.users_list = []
+
+
+if __name__ == '__main__':
+    unittest.main()
