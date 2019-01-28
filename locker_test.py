95a2a935eb85cde8c39d599795b4c32b068538f7
James Mwangi <james_mwangi@aol.com>
2019-01-28 23:12:15 +0300

    add the setup test and the save user test

--- locker_test.py
+++ locker_test.py
@@ -17,7 +17,29 @@ class TestLocker(unittest.TestCase):
 
         def save_user(self):
             User.users_list.append(self)
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_user = User("Jamesnduge", "tj193345")
 
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_user.username,"Jamesnduge")
+        self.assertEqual(self.new_user.password,"tj193345")
+
+    def test_save_user(self):
+        '''
+        test_save_user test case to test if the user object is saved into
+         the users_list
+        '''
+        self.new_user.save_user()
+        self.assertEqual(len(User.users_list),1)
+
+    
 
 if __name__ == '__main__':
     unittest.main()
