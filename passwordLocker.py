44905b4f5dfceb1cdfbc4f3b4b430af3e9072dae
James Mwangi <james_mwangi@aol.com>
2019-01-29 11:23:18 +0300

    add a class method to display all saved accounts

--- passwordLocker.py
+++ passwordLocker.py
@@ -1,5 +1,10 @@
 class User:
     users_list = []
+    """
+    Args:
+    username = new username
+    password = new password
+    """
 
     def __init__(self, username, password):
         self.username = username
@@ -12,10 +17,26 @@ class User:
 
 class Credentials:
     accounts_list = []
+    """
+    Args:
+    account_name = new account_name
+    user_name = new user_name
+    key = new password
+    """
     def __init__(self, account_name, user_name, key):
         self.account_name = account_name
         self.user_name = user_name
         self.key = key
 
     def save_account(self):
-            Credentials.accounts_list.append(self)
+        Credentials.accounts_list.append(self)
+
+    def delete_account(self):
+        Credentials.accounts_list.remove(self)
+
+    @classmethod
+    def display_accounts(cls):
+        '''
+        method that returns the accounts list
+        '''
+        return cls.accounts_list
